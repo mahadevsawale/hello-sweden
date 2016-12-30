2d0aec06fd1bfd0931bbca3095621f7787f87263
Mahadev Sawale <Mahadev.Sawale@gmail.com>
2016-12-30 16:37:17 +0100

    Fix failing test

--- src/test/java/com/hello/HelloTest.java
+++ src/test/java/com/hello/HelloTest.java
@@ -14,10 +14,10 @@ public class HelloTest {
     }
 
     @Test
-    public void testSayHelloFail() {
+    public void testSayHello2() {
         final String expectedHello = "Hello India!!!";
         final String actualHello = Hello.sayHello("Sweden");
 
-        Assert.assertEquals(expectedHello, actualHello);
+        Assert.assertNotEquals(expectedHello, actualHello);
     }
 }
